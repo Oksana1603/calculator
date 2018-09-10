889f8c5d61a8e4713b74a5b10fe6f6222061723c
Oksana <oksana16zahorodniuk@gmail.com>
2018-09-10 14:19:14 +0000

    remade

--- js/index.js
+++ js/index.js
@@ -4,33 +4,39 @@ const form = document.getElementById('form');
 performMath.addEventListener('click', () => {
 
 
-    let func = form.func.value;
-    let num1 = form.num1.value;
-    let num2 = form.num2.value;
+    const func = form.func.value;
+    const num1 = parseFloat(form.num1.value);
+    const num2 = parseFloat(form.num2.value);
     //console.log(func, num1, num2);
     event.preventDefault();
     let answer = '';
-    switch(func){
-     case "+":    
-        answer = (parseFloat(num1))+(parseFloat(num2));
-     break;   
-    case "-":    
-        answer = (parseFloat(num1))-(parseFloat(num2));
-     break; 
-     case "*":    
-        answer = (parseFloat(num1))*(parseFloat(num2));
-     break; 
-     case "/":    
-        answer = (parseFloat(num1))/(parseFloat(num2));
-     break; 
-     case "root":    
-        answer = Math.sqrt(parseFloat(num1));
-     break; 
+    switch (func) {
+        case "+":
+            answer = num1 + num2;
+            break;
+        case "-":
+            answer = num1 - num2;
+            break;
+        case "*":
+            answer = num1 * num2;
+            break;
+        case "/":
+            answer = num1 / num2;
+            break;
+        case "root":
+            answer = Math.sqrt(num1);
+            break;
     }
-const section = document.querySelector('section');
-const div = document.createElement('div');
-section.appendChild(document.createTextNode('Your answer is '+ answer+ '.'+'\n') );
+    const section = document.querySelector('section');
+    const div = document.createElement('div');
+    section.appendChild(document.createTextNode('Your answer is ' + answer + '.' + '\n'));
 
 
 });
-
+const section = document.querySelector('section');
+const div = document.createElement('div');
+const clear = document.querySelector('.form#clear');
+clear.addEventListener('click', ()=>{
+section.removeChild(div);   
+    
+})
